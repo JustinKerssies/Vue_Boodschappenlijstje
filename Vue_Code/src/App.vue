1afdf7161256ba69526365f980bd23a988e4cbab
JustinKerssies <jb.kerssies@gmail.com>
2026-01-19 10:15:53 +0100

    update

---
+++ Vue_Code/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {ref} from 'Vue'
+
+const groceryArray = ref([
+  {'index' : 1, "product": "cups", "bought": False},
+  {'index' : 2, "product": "hats", "bought": False},
+  {'index' : 3, "product": "vibes", "bought": False},
+])
+</script>
+
+<template>
+<ul>
+  <li v-for="value in groceryArray" >
+    <p> {{ value.product }}</p>
+  </li>
+</ul>
+</template>
+
+<style scoped>
+
+</style>
