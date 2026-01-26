34a1325d611388283472ad61c34f6a4605ddeb85
JustinKerssies <jb.kerssies@gmail.com>
2026-01-26 11:42:04 +0100

    shenanigans

--- Vue_Code/src/App.vue
+++ Vue_Code/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import {ref, computed} from 'vue'
+import GroceriesTable from './components/GroceriesTable.vue'
+import {ref, watch} from 'vue'
 
+const removeMarker = ref(false)
 const inputProduct = ref('')
 const inputValue = ref(0)
 const indexMarker = ref(3)
-const removeMarker = ref(false)
 const grocerylist = ref([
   {"index": 1, "product": "cups", "value": 1.99, "amount": 5, },
   {"index": 2, "product": "hats", "value": 15, "amount": 4, },
@@ -24,49 +25,22 @@ const addToList = () => {
   }
 }
 
-const removeFromList = (product) => {
+const removeFromList = product => {
   grocerylist.value = grocerylist.value.filter(obj => obj != product)
-}
-
-const subTotalValue = (item) => {
-  return item['amount']  * item['value']
 }
 const removeTrigger = () => {
   removeMarker.value = !removeMarker.value
 }
 
-const sumTotal = computed(() => {
-  let sum = 0
-  for (let index = 0; index < grocerylist.value.length; index++) {
-    sum += subTotalValue(grocerylist.value[index]); 
-  } return sum
-})
 </script>
 
 <template>
-  <table>
+   <table>
     <td> product <input type="text" v-model="inputProduct"/> </td>
     <td> price <input type="number" v-model="inputValue"/> </td>
     <td> <button v-on:click="addToList()"> add</button></td>
   </table>
-  <table>
-    <tr>
-      <th>Index</th>
-      <th>product</th>
-      <th>price</th>
-      <th>amount</th>
-      <th>subTotal</th>
-    </tr>
-    <tr v-for="item in grocerylist">
-      <td> {{ item.index }} </td>
-      <td> {{ item.product }} </td>
-      <td> {{ item.value }} </td>
-      <td> <input type="number" v-model.number="item.amount"></td>
-      <td> {{ subTotalValue(item) }} </td>
-      <td> <button type="button" v-on:click="removeFromList(item)" v-if="removeMarker">x</button></td>
-    </tr>
-  </table>
-  <p> {{ sumTotal }}</p>
+  <GroceriesTable :listProp="grocerylist" :removeProp="removeMarker" @removeProduct="removeFromList"> </GroceriesTable>
   <td> <button v-on:click="removeTrigger()">remove</button></td>
 </template>
 
